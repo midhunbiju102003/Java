3b311e38eebdb8c482d55dc51d8dd0adb98a8d0e
Midhun Biju <166794539+midhunbiju102003@users.noreply.github.com>
2025-02-28 14:10:16 +0530

    Add files via upload

---
+++ Complex.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Complex {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the real and imaginary part of first complex number:");
+        double real1=sc.nextDouble();
+        double img1=sc.nextDouble();
+
+        System.out.print("Enter real and imaginary part of second complex number:");
+        double real2=sc.nextDouble();
+        double img2=sc.nextDouble();
+
+        double sumreal = real1+real2;
+        double sumimg = img1+img2;
+        System.out.println("Sum="+sumimg+"+"+sumreal+"i");
+        sc.close();
+    }
+    
+}
