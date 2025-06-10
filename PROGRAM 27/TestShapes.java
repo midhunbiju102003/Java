ce662c67cfa1801522aa438caeeb86c270f8be34
Midhun Biju <166794539+midhunbiju102003@users.noreply.github.com>
2025-06-10 21:36:33 +0530

    Update TestShapes.java

--- PROGRAM 27/TestShapes.java
+++ PROGRAM 27/TestShapes.java
@@ -1,19 +1,18 @@
 import Graphics.Rectangle;
-import Graphics.Traingle;
+import Graphics.Triangle;  
 import Graphics.Square;
 import Graphics.Circle;
 
-public class TestShapes{
-    public static void main(String[] args){
-        Rectangle rectangle=new Rectangle(5, 3);
-        Traingle traingle=new Traingle(4, 6);
-        Square square=new Square(4);
-        Circle circle=new Circle(3);
+public class TestShapes {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 3);
+        Triangle triangle = new Triangle(4, 6);
+        Square square = new Square(4);
+        Circle circle = new Circle(3);
 
-        System.out.println("Area of Rectangle:"+rectangle.calculateArea());
-        System.out.println("Area of Traingle:"+traingle.calculateArea());
-        System.out.println("Area of Square:"+square.calculateArea());
-        System.out.println("Area of Circle:"+circle.calculateArea());
-
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+        System.out.println("Area of Triangle: " + triangle.calculateArea());
+        System.out.println("Area of Square: " + square.calculateArea());
+        System.out.println("Area of Circle: " + circle.calculateArea());
     }
 }
